8fed4263087499ebfc861edcd94cd77236b42e8b
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-03-31 12:51:42 +0300

    formating configs

--- gem/gem-meeting-web-client/.eslintrc.js
+++ gem/gem-meeting-web-client/.eslintrc.js
@@ -1,11 +1,8 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    '@vue/airbnb',
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/airbnb'],
   rules: {
-    "no-param-reassign": ["error", { "props": false }],
-    "no-debugger": 0
+    'no-param-reassign': ['error', { props: false }],
+    'no-debugger': 0,
+    'comma-dangle': ['error', 'never']
   }
 };
