45b8296e2fa2833d3e1ea70588a1a1ed6828cde6
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-04-07 13:02:14 +0300

    login form

--- gem/frontend/gem-portal/pages/login.vue
+++ gem/frontend/gem-portal/pages/login.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="login">
-    <h1>This is an login page</h1>
-    <input v-model="name">
-    <input v-model="password">
-    <input
-      type="submit"
-      @click="signIn">
-  </div>
+  <section class="hero is-info is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <h1 class="title">
+            GBC Environment For Meetings
+          </h1>
+          <div class="box">
+            <div class="field">
+              <p class="control is-expanded">
+                <input v-model="name" class="input" type="text" placeholder="Name">
+              </p>
+            </div>
+            <div class="field">
+              <p class="control is-expanded">
+                <input v-model="password" class="input" type="text" placeholder="Password">
+              </p>
+            </div>
+            <div class="field">
+              <p class="control is-expanded">
+                <a @click="signIn" class="button is-fullwidth is-primary">Login</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -20,17 +40,45 @@ export default {
   },
   methods: {
     async signIn() {
-      const res = await this.$axios.post('http://localhost:5000/login', {
-        name: this.name,
-        password: this.password
-      });
-      if (res.data.success) {
-        localStorage.token = res.data.token;
-      } else {
-        alert("It isn't you bro!");
+      try {
+        const res = await this.$axios.post('http://localhost:5000/login', {
+          name: this.name,
+          password: this.password
+        });
+        if (res.data.success) {
+          localStorage.token = res.data.token;
+          this.$router.push('/meeting');
+        } else {
+          alert("It isn't you bro!");
+        }
+      } catch (e) {
+        alert('Some error!');
       }
     }
   }
 };
 </script>
 
+<style scoped>
+html,
+body {
+  font-family: 'Open Sans', serif;
+}
+.hero.is-info {
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url('/login-bg/bg1.jpg') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.hero .nav,
+.hero.is-success .nav {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.hero .subtitle {
+  padding: 3rem 0;
+  line-height: 1.5;
+}
+</style>
