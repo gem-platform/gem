b3d492855f96ee54524ced4d7e233c74865b82cb
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-04-16 18:46:47 +0300

    prepare for code cleanup

--- gem/frontend/gem-portal/.eslintrc.js
+++ gem/frontend/gem-portal/.eslintrc.js
@@ -11,11 +11,16 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential'
+    // 'eslint:recommended',
+    // 'plugin:vue/recommended',
+    // '@vue/airbnb'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'no-param-reassign': 0, // ['error', { props: false }],
+    'no-debugger': 0,
+    'comma-dangle': ['error', 'never']
+  }
+};
