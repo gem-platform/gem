239db7b77d84db6390d6a3477f82bf473efa6cf2
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-04-19 17:07:26 +0300

    progressbar

--- gem/frontend/gem-portal/nuxt.config.js
+++ gem/frontend/gem-portal/nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: 'whitesmoke', height: '4px' },
   /*
   ** Build configuration
   */
