b28d3bab80fbe90795fa6230b94b3e9a34b66c9d
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-06-24 12:09:30 +0300

    Log out doesn't work #11

--- gem/frontend/gem-portal/components/Navbar.vue
+++ gem/frontend/gem-portal/components/Navbar.vue
@@ -71,11 +71,6 @@
             <b-dropdown-item has-link>
               <nuxt-link
                 class="navbar-item"
-                to="/login">
-                Login
-              </nuxt-link>
-              <nuxt-link
-                class="navbar-item"
                 to="/logout">
                 Logout
               </nuxt-link>
