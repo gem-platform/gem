000d786cb77825362be2680d4131912f2f4e68d0
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-07-01 16:43:23 +0300

    #82 za

--- gem/backend/gem-office-server/main.py
+++ gem/backend/gem-office-server/main.py
@@ -1,10 +1,22 @@
+from os import environ
 from io import BytesIO
 from requests import post
 from flask import Flask, jsonify
+from mongoengine import connect
 
 from reports.za import zonal_assignments_report
 
 app = Flask("office")
+
+db_host = environ.get('DB_HOST', "localhost")
+db_username = environ.get('MONGO_USERNAME')
+db_password = environ.get('MONGO_PASSWORD')
+db_auth_source = environ.get('MONGO_AUTH_SOURCE')
+db_auth_mechanism = environ.get('MONGO_AUTH_MECHANISM')
+connect("gem",
+        host=db_host, username=db_username, password=db_password,
+        authentication_source=db_auth_source,
+        authentication_mechanism=db_auth_mechanism)
 
 
 def print_and_save(content):
@@ -24,4 +36,4 @@ def generate_zonal_report():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001, host="0.0.0.0")
--- gem/backend/gem-office-server/reports/za/zonal_assignments.py
+++ gem/backend/gem-office-server/reports/za/zonal_assignments.py
@@ -1,7 +1,42 @@
+from itertools import groupby
+
 from jinja2 import Template
+from gem.db.models import Official, Assignment
 
 
 def zonal_assignments_report():
-    template = Template('Hello {{ name }}!')
-    report = template.render(name='John Doe')
-    return report
+    report = []
+    for official in Official.objects.all():
+        assignments = Assignment.objects(officials__contains=official)
+        if not assignments:
+            continue
+
+        report_record = {"name": official.semi_formal_name(), "groups": []}
+
+        assignments = sorted(assignments, key=lambda x: (len(x.zone.assignees), x.zone.assignees))
+        groups = groupby(assignments, key=lambda x: x.zone.assignees)
+
+        for assignees, assignments in groups:
+            assignees_without_self = list(filter(lambda x: x != official, assignees))
+            cozonal = True if assignees_without_self else False
+            cozonal_names = list(map(lambda a: a.semi_formal_name(), assignees_without_self))
+
+            report_record_group = {"with": cozonal_names, "zones": []}
+
+            rz = []
+            for assignment in assignments:
+                for child_zone in assignment.zone.children:
+                    rz.append(child_zone)
+
+            rz = sorted(rz)
+            for r in rz:
+                report_record_group["zones"].append(r.name)
+
+            report_record["groups"].append(report_record_group.copy())
+        report.append(report_record)
+
+    # return generated html
+    template_file = open("./za.html", "r").read()
+    template = Template(template_file)
+    report_html = template.render(data=report)
+    return report_html
