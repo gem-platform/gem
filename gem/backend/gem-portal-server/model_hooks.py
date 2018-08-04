40978c96a56fc71feb242c03c65aed2ba6e5547c
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-08-04 17:34:16 +0300

    fix password issue

--- gem/backend/gem-portal-server/model_hooks.py
+++ gem/backend/gem-portal-server/model_hooks.py
@@ -4,8 +4,17 @@ from gem.db.models import Zone
 
 
 def user_replace_password(item, original):
-    if not item.get("password", None):
-        item["password"] = original.get("password", None)
+    db = current_app.data.driver.db
+    new_password = item.get("password", None)
+    if not new_password:  # no new password provided, get of from original
+        # original["password"] is not working here, because
+        # it is removed by projection (defined in settings.py)
+        user = db["users"].find_one({"_id": item["_id"]})
+        if user:
+            item["password"] = user.get("password", None)
+    else:
+        # todo: hash and salt password
+        pass
 
 
 def __get_parents_path(zone, result):
@@ -28,4 +37,3 @@ def zone_update_path_items(items):
     for item in items:
         zone = Zone.objects.get(id=ObjectId(item["_id"]))
         zone.save()  # call mongoengine hooks
-
