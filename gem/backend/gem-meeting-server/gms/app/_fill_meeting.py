767391ae3fcaab52d4dc4a6712704b5a2c66d5c9
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-08-14 13:22:51 +0300

    cleanup

--- gem/backend/gem-meeting-server/gms/app/_fill_meeting.py
+++ gem/backend/gem-meeting-server/gms/app/_fill_meeting.py
@@ -60,37 +60,3 @@ def add_group(meeting, proposal):
     meeting.stages.append(CommentsMeetingStage(comments, group=group))
     meeting.stages.append(DiscussionMeetingStage(group=group))
     meeting.proposals.append(proposal)
-
-
-def init_db():
-    prop1 = Proposal()
-    prop1.title = "First Proposal"
-    prop1.content = "Content of proposal"
-    prop1.index = "P001"
-
-    role1 = Role()
-    role1.name = "Tester"
-    role1.permissions = ["*", "vote", "comment", "discuss"]
-
-    user1 = User()
-    user1.name = "Tester das"
-    user1.roles.append(role1)
-    user1.password = "tester"
-
-    perm1 = MeetingPermission()
-    perm1.name = "meeting.join"
-    perm1.role = role1
-    perm1.user = user1
-
-    meet1 = Meeting()
-    meet1.title = "AGM 2018"
-    meet1.agenda = "Agenda of testing meeting"
-    meet1.proposals.append(prop1)
-    meet1.permissions.append(perm1)
-
-    prop1.save()
-    role1.save()
-    user1.save()
-    meet1.save()
-
-    return meet1
