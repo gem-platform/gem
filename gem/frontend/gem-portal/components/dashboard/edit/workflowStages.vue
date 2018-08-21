79e8e0120cac8fab50a19f53a7d72b3cee4d4e6e
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-08-21 14:29:06 +0300

    #107 workflow pages

---
+++ gem/frontend/gem-portal/components/dashboard/edit/workflowStages.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- Name -->
+    <b-field
+      label="Name">
+      <b-input
+        v-model.trim="name"
+        placeholder="Name"
+        size="is-large" />
+    </b-field>
+
+    <!-- Description -->
+    <b-field
+      label="Description">
+      <textarea
+        v-model="description"
+        class="textarea"
+        placeholder="Description"/>
+    </b-field>
+  </div>
+</template>
+
+<script>
+import CrudEditComponentMixin from '@/components/CrudEditComponentMixin';
+
+export default {
+  mixins: [
+    CrudEditComponentMixin({ properties: ['name', 'description'] })
+  ],
+  props: {
+    entity: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
