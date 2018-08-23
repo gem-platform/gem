403f3b382f1a13db00c9c7548fea68fc63219e52
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-08-23 12:29:30 +0300

    #108 travis script

--- gem/backend/gem-meeting-server/tests/test_votes.py
+++ gem/backend/gem-meeting-server/tests/test_votes.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('.')
 sys.path.append("../gem-server-common")
 
-from gem.db import *
+from gem.db import Proposal, Ballot, Role, User, OpForbidden
 
 from gms.meeting import Meeting
 from gms.meeting.stages import *
@@ -14,39 +14,34 @@ from gms.app._fill_meeting import add_group
 
 def test_user_role_in_votes_persistent():
     meeting = Meeting(context=Context())
-    
+
     proposal = Proposal()
-    ballot = Ballot(proposal = proposal)
-    
-    role1 = Role()   
+    ballot = Ballot(proposal=proposal)
+
+    role1 = Role()
     role2 = Role()
-    
+    role1.priority = 1
+    role2.priority = 5
+
     user = User()
     user.roles.append(role2)
 
     ballot.set(user, True)
-    assert(len(ballot.votes[0].roles) == 1)
-    assert(ballot.votes[0].roles[0] == role2)
-    
+    assert ballot.votes[0].role == role2
+
+    # user already voted, so no role should be changed
     user.roles.append(role1)
-    assert(len(ballot.votes[0].roles) == 1)
-    assert(ballot.votes[0].roles[0] == role2)
-
-    ballot.set(user, False)
-    assert(len(ballot.votes[0].roles) == 2)
-
-    user.roles.pop()
-    assert(len(ballot.votes[0].roles) == 2)
+    assert ballot.votes[0].role == role2
 
 
-def test_unapprove_vote_in_finished_meeting():
+def test_unapproved_vote_in_finished_meeting():
     meeting = Meeting(context=Context())
-    
+
     proposal = Proposal()
-    ballot = Ballot(proposal = proposal)
+    ballot = Ballot(proposal=proposal)
     ballot.finished = True
 
     user = User()
     with pytest.raises(OpForbidden, match="Ballot is finished already."):
         ballot.set(user, False)
-    
+
