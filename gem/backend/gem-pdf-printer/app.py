1e04e88e129f4308a0953472b99802d4856510c5
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-08-24 15:09:45 +0300

    cleanup

--- gem/backend/gem-pdf-printer/app.py
+++ gem/backend/gem-pdf-printer/app.py
@@ -1,7 +1,10 @@
+"""PDF printing service."""
+
 from logging import debug
+from logging.config import fileConfig
+
 from weasyprint import HTML
 from flask import Flask, request, make_response
-from logging.config import fileConfig
 
 fileConfig('logging.conf')
 app = Flask('pdf')
@@ -9,12 +12,17 @@ app = Flask('pdf')
 
 @app.route('/pdf', methods=['POST'])
 def generate():
+    """Generate PDF using specified data."""
+    # get request data
     name = request.args.get('filename', 'unnamed.pdf')
-    debug("Printing pdf '%s'", name)
-
     file_data = request.files["file"].read()
+
+    # generate pdf file
+    debug("Printing pdf '%s'", name)
     html = HTML(string=file_data)
     pdf = html.write_pdf()
+
+    # send response
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline;filename=%s' % name
