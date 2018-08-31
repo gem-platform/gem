b1270e1b733f15fc03f13cb329215fff3ff694c2
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-08-31 11:02:16 +0300

    #1 tests: secret ballot

--- gem/backend/gem-meeting-server/tests/meeting/test_votes.py
+++ gem/backend/gem-meeting-server/tests/meeting/test_votes.py
@@ -3,31 +3,21 @@ from gem.db import Proposal, Ballot, Role, User, OpForbidden
 
 from tools import drop_db
 
+
 def teardown_function():
     drop_db()
 
 
-def test_user_role_in_votes_persistent():
+def test_user_role_saved(user):
+    """User role should be saved."""
     proposal = Proposal()
     ballot = Ballot(proposal=proposal)
-
-    role1 = Role()
-    role2 = Role()
-    role1.priority = 1
-    role2.priority = 5
-
-    user = User()
-    user.roles.append(role2)
-
-    ballot.set(user, True)
-    assert ballot.votes[0].role == role2
-
-    # user already voted, so no role should be changed
-    user.roles.append(role1)
-    assert ballot.votes[0].role == role2
+    ballot.set(user, "yes")
+    assert ballot.votes[0].role == user.roles[0]
 
 
 def test_right_role_priority():
+    """Right role (with maximum priority) should be saved for ballot."""
     proposal = Proposal()
     ballot = Ballot(proposal=proposal)
 
@@ -44,7 +34,8 @@ def test_right_role_priority():
     assert ballot.votes[0].role == role1
 
 
-def test_unapproved_vote_in_finished_meeting(user):
+def test_unable_to_vote_for_finished_ballot(user):
+    """Unable to vote for finished ballot"""
     proposal = Proposal()
     ballot = Ballot(proposal=proposal, finished=True)
 
@@ -52,12 +43,21 @@ def test_unapproved_vote_in_finished_meeting(user):
         ballot.set(user, False)
 
 
-def test_userid_is_not_saved_for_secret_ballot(user):
+def test_user_is_not_saved_for_secret_ballot(proposal, user):
     """User is should not be saved for secret ballot."""
-    proposal = Proposal()
     ballot = Ballot(proposal=proposal, secret=True)
-    ballot.set(user, True)
+    ballot.set(user, "yes")
     ballot.save()
 
     for vote in ballot.votes:
         assert vote.user is None
+
+
+def test_user_saved_for_open_ballot(proposal, user):
+    """User should be saved for open ballot."""
+    ballot = Ballot(proposal=proposal, secret=False)
+    ballot.set(user, "yes")
+    ballot.save()
+
+    for vote in ballot.votes:
+        assert vote.user is user
