c37cd4c73e545e5465b01f832d37e1cc2ed42bfb
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-09-11 08:36:09 +0300

    db again test

--- gem/backend/gem-meeting-server/tests/conftest.py
+++ gem/backend/gem-meeting-server/tests/conftest.py
@@ -3,7 +3,7 @@ from mongoengine import connect
 from tools import drop_db
 
 connect("gem_test_",
-        host="dbs", username="bhagavan",
+        host="db", username="bhagavan",
         password="UZz5dGzZn@R*j\9%",
         authentication_source="admin",
         authentication_mechanism="SCRAM-SHA-1")
