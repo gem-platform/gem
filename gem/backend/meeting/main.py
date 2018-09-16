f3cec6c916fa3bcdbcf835498f2a3335432263e8
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-09-16 09:16:14 +0300

    codestyle

--- gem/backend/meeting/main.py
+++ gem/backend/meeting/main.py
@@ -9,15 +9,15 @@ from gms.app import MeetingServerApplication
 from gms.net import SocketIoEndpoint
 
 # setup logging
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig("logging.conf")
 logging.getLogger("root").info("GEM Meeting Server is starting")
 
 # connect to DB
-DB_HOST = os.environ.get('DB_HOST', "localhost")
-DB_USERNAME = os.environ.get('MONGO_USERNAME')
-DB_PASSWORD = os.environ.get('MONGO_PASSWORD')
-DB_AUTH_SOURCE = os.environ.get('MONGO_AUTH_SOURCE')
-DB_AUTH_MECHANISM = os.environ.get('MONGO_AUTH_MECHANISM')
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_USERNAME = os.environ.get("MONGO_USERNAME")
+DB_PASSWORD = os.environ.get("MONGO_PASSWORD")
+DB_AUTH_SOURCE = os.environ.get("MONGO_AUTH_SOURCE")
+DB_AUTH_MECHANISM = os.environ.get("MONGO_AUTH_MECHANISM")
 connect("gem",
         host=DB_HOST, username=DB_USERNAME, password=DB_PASSWORD,
         authentication_source=DB_AUTH_SOURCE,
