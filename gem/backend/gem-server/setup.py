53196e57e13f8959ff59a3953f09a862b78ab4ec
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2018-12-05 15:44:59 +0300

    cleanup

--- gem/backend/gem-server/setup.py
+++ gem/backend/gem-server/setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(
-    name='gem',
-    version='1.0',
-    description='Common classes for GEM Platform',
-    author='Advaita Krishna das',
-    packages=['gem', 'gem.core', 'gem.db'],
+    name="gem",
+    version="1.0",
+    description="Common classes for GEM Platform",
+    author="Advaita Krishna das",
+    packages=["gem", "gem.core", "gem.db", "gem.utils"],
 )
--- gem/backend/meeting/main.py
+++ gem/backend/meeting/main.py
@@ -4,7 +4,7 @@ import json
 import logging
 import logging.config
 
-from mongoengine import connect
+from gem.utils.db import connect_db
 
 from gms.app import MeetingServerApplication
 from gms.net import SocketIoEndpoint, HealthEndpoint
@@ -16,16 +16,7 @@ with open("logging.json", "r", encoding="utf-8") as fd:
 # we are starting
 logging.getLogger("root").info("GEM Meeting Server is starting")
 
-# connect to DB
-DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_USERNAME = os.environ.get("MONGO_USERNAME")
-DB_PASSWORD = os.environ.get("MONGO_PASSWORD")
-DB_AUTH_SOURCE = os.environ.get("MONGO_AUTH_SOURCE")
-DB_AUTH_MECHANISM = os.environ.get("MONGO_AUTH_MECHANISM")
-connect("gem",
-        host=DB_HOST, username=DB_USERNAME, password=DB_PASSWORD,
-        authentication_source=DB_AUTH_SOURCE,
-        authentication_mechanism=DB_AUTH_MECHANISM)
+connect_db()
 
 # configure application endpoints
 MEETING_ENDPOINT = SocketIoEndpoint("0.0.0.0", 8090)
