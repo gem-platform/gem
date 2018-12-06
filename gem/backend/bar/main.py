8e7e54431b7b63400c679557f0af4f45cac2c628
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2018-12-06 16:06:55 +0300

    bar service cleanup

--- gem/backend/bar/main.py
+++ gem/backend/bar/main.py
@@ -1,5 +1,5 @@
-import uuid
 import logging
+from uuid import uuid4
 from socketio import AsyncServer
 from aiohttp import web
 
@@ -10,36 +10,31 @@ sio.attach(app)
 orders = []
 
 
-@sio.on('orders')
+@sio.on("orders")
 async def orders_msg(sid):
     return orders
 
-@sio.on('order')
+@sio.on("order")
 async def message(sid, data):
     user = data.get("name", "<Unknown>")
     items = data.get("items", [])
 
     order = {
-        "id": str(uuid.uuid4()),
+        "id": str(uuid4()),
         "name": user,
         "items": [{"name": item["name"]} for item in items]
     }
 
     orders.append(order)
-
-    logging.getLogger("root").critical(order)
-
     await sio.emit("add_order", order)
-
     return {"success": True}
 
-@sio.on('done')
+@sio.on("done")
 async def message_done(sid, data):
     global orders
-    id = data.get("id")
-    orders = list(filter(lambda x: x["id"] != id, orders))
+    order_id = data.get("id")
+    orders = list(filter(lambda x: x["id"] != order_id, orders))
     return {"success": True}
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     web.run_app(app, port=8090)
